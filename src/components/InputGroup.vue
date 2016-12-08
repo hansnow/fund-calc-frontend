4618c2181b33d75615fd7a5bf1479034e0fccc86
hansnow <hansnow2012@gmail.com>
2016-12-08 09:51:55 +0800

    add fund name hit

--- src/components/InputGroup.vue
+++ src/components/InputGroup.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="w3-container">
-    <div class="w3-half">
+    <div class="w3-third">
+      <div class="w3-group w3-margin">
+        <label class="w3-label">基金名称</label>
+        <p v-if="loading"><img src="../../static/spinner_squares.svg" style="width: 24px"></p>
+        <p v-else>{{name}}</p>
+      </div>
+    </div>
+    <div class="w3-third">
       <div class="w3-group w3-margin">
         <label class="w3-label">基金代码</label>
         <input :value="code" @input="changeCode($event.target.value)" class="w3-input" type="text">
       </div>
     </div>
-    <div class="w3-half">
+    <div class="w3-third">
       <div class="w3-group w3-margin">
         <label class="w3-label">份额</label>
         <input :value="amount" @input="changeAmount($event.target.value)" class="w3-input" type="text">
@@ -16,15 +23,46 @@
 </template>
 
 <script>
+  import axios from 'axios'
   export default {
     name: 'input_group',
+    data () {
+      return {
+        name: '',
+        loading: false
+      }
+    },
     props: ['index', 'code', 'amount'],
     methods: {
       changeCode (newCode) {
         this.$emit('changeCode', this.index, newCode)
+        if (newCode.length === 6) {
+          this.getFundName(newCode)
+        }
       },
       changeAmount (newAmount) {
         this.$emit('changeAmount', this.index, newAmount)
+      },
+      getFundName (fundCode) {
+        this.loading = true
+        axios.post('/api/get_fund_name', {fund: fundCode})
+        .then((resp) => {
+          this.loading = false
+          if (resp.data) {
+            this.name = resp.data
+          } else {
+            this.name = 'Not Found'
+          }
+        })
+        .catch((err) => {
+          this.loading = false
+          this.name = `Network Error: ${err}`
+        })
+      }
+    },
+    created () {
+      if (this.code.length === 6) {
+        this.getFundName(this.code)
       }
     }
   }
