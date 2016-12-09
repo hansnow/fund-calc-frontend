4f3a3afae33d0732e5bfce370d79ceb16b4201ce
hansnow <hansnow2012@gmail.com>
2016-12-09 10:12:27 +0800

    add component: Chart

--- src/util.js
+++ src/util.js
@@ -10,5 +10,52 @@ export default {
     clear () {
       localStorage.clear()
     }
+  },
+  chart: {
+    horizonalLinePlugin: {
+      afterDraw: function (chartInstance) {
+        var yScale = chartInstance.scales['y-axis-0']
+        var canvas = chartInstance.chart
+        var ctx = canvas.ctx
+        var index
+        var line
+        var style
+        var yValue
+
+        if (chartInstance.options.horizontalLine) {
+          for (index = 0; index < chartInstance.options.horizontalLine.length; index++) {
+            line = chartInstance.options.horizontalLine[index]
+
+            if (!line.style) {
+              style = 'rgba(169,169,169, .6)'
+            } else {
+              style = line.style
+            }
+
+            if (line.y) {
+              yValue = yScale.getPixelForValue(line.y)
+            } else {
+              yValue = 0
+            }
+
+            ctx.lineWidth = 3
+
+            if (yValue) {
+              ctx.beginPath()
+              ctx.moveTo(0, yValue)
+              ctx.lineTo(canvas.width, yValue)
+              ctx.strokeStyle = style
+              ctx.stroke()
+            }
+
+            if (line.text) {
+              ctx.fillStyle = style
+              ctx.fillText(line.text, 0, yValue + ctx.lineWidth)
+            }
+          }
+          return
+        }
+      }
+    }
   }
 }
